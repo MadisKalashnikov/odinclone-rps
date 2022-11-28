bceed599d25be42bb8b20f846ad864db6bbfb7ca
Madis <kalashnikov.madis@gmail.com>
2022-11-28 13:39:30 +0200

    Add missing commas

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const logStyles = {
 	finalResult: [
 		"font-size: 17px",
 		"border-bottom: 2px double black",
-	].join(";"),
-}
+	].join(";")
+};
 
 let userScore = 0;
 let computerScore = 0;
@@ -36,10 +36,10 @@ function logChoices(userChoice, computerChoice, whoWon) {
 	if (whoWon === "draw") {
 		console.log(`%cA draw! both players chose ${userChoice}`, logStyles.choiceStyle)
 	} else if (whoWon === "user") {
-		console.log(`%cusers ${userChoice} beats computers ${computerChoice}`, logStyles.choiceStyle)
+		console.log(`%cusers ${userChoice} beats computers ${computerChoice}`, logStyles.choiceStyle);
 	} else if (whoWon === "computer") {
-		console.log(`%ccomputers ${computerChoice} beats users ${userChoice}`, logStyles.choiceStyle)
-	}
+		console.log(`%ccomputers ${computerChoice} beats users ${userChoice}`, logStyles.choiceStyle);
+	};
 };
 function logResultOfRound(userScore, computerScore) {
 	console.log(`%cResult of the round - User: ${userScore}, Computer: ${computerScore}`, logStyles.roundResult);
@@ -53,33 +53,33 @@ function resetScores() {
 function playARound(userChoice, computerChoice) {
 	if (userChoice === "paper" && computerChoice === "rock") {
 		userScore += 1;
-		logChoices(userChoice, computerChoice, "user")
+		logChoices(userChoice, computerChoice, "user");
 		logResultOfRound(userScore, computerScore);
 	} else if (userChoice === "rock" && computerChoice === "scissors") {
 		userScore += 1;
-		logChoices(userChoice, computerChoice, "user")
+		logChoices(userChoice, computerChoice, "user");
 		logResultOfRound(userScore, computerScore);
 	} else if (userChoice === "scissors" && computerChoice === "paper") {
 		userScore += 1;
-		logChoices(userChoice, computerChoice, "user")
+		logChoices(userChoice, computerChoice, "user");
 		logResultOfRound(userScore, computerScore);
 	} else if (userChoice === computerChoice) {
-		logChoices(userChoice, computerChoice, "draw")
+		logChoices(userChoice, computerChoice, "draw");
 		logResultOfRound(userScore, computerScore);
 	} else {
 		computerScore += 1;
-		logChoices(userChoice, computerChoice, "computer")
+		logChoices(userChoice, computerChoice, "computer");
 		logResultOfRound(userScore, computerScore);
 	};
 };
 function playGame() {
 	if (userScore !== 0 || computerScore !== 0) {
-		resetScores()
-	}
+		resetScores();
+	};
 	for (let i = 0; i < 5; i++) {
 		console.log(`%cRound ${i + 1}`, logStyles.roundStyle)
 		playARound(getUserChoice(), getComputerChoice());
-	}
+	};
 	if (userScore > computerScore) {
 		console.log("%cFinal result - User won!", logStyles.finalResult);
 	} else if (computerScore > userScore) {
